b3fe87d7fda74a1694d34fe694d65f2172e9043d
kugimiya <bp.001@yandex.ru>
2018-04-01 00:51:58 +0400

    Pentagon fabric

---
+++ Fabric/PentagonFabric.cpp
@@ -0,0 +1,7 @@
+#include "PentagonFabric.h"
+
+Pentagon PentagonFabric::GetObject()
+{
+    Pentagon DummyPentagon = Pentagon({10, 10}, {0, -3}, {3, 0}, {2, 4}, {-2, 4}, {-3, 0});
+    return DummyPentagon;
+}
--- Fabric/PentagonFabric.h
+++ Fabric/PentagonFabric.h
@@ -5,13 +5,8 @@
 class PentagonFabric
 {
   public:
-    PentagonFabric() {}
-    ~PentagonFabric() {}
+    PentagonFabric() = default;
+    ~PentagonFabric() = default;
     static Pentagon GetObject();
 };
 
-Pentagon PentagonFabric::GetObject()
-{
-    Pentagon DummyPentagon = Pentagon({10, 10}, {0, -3}, {3, 0}, {2, 4}, {-2, 4}, {-3, 0});
-    return DummyPentagon;
-}
